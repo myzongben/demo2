68f942764f0383ba79eca501d7d0376186792878
wangzongben <74747181+myzongben@users.noreply.github.com>
2021-08-06 17:48:13 +0800

    work: 	1.新增用户评论功能后台实现（1） question&solution      注意Springboot接受前端传递参数的方式

--- src/main/java/com/example/wzb/controller/JiyixianController.java
+++ src/main/java/com/example/wzb/controller/JiyixianController.java
@@ -4,15 +4,18 @@ import com.example.wzb.entity.Jiyixian;
 import com.example.wzb.paramDTO.Result;
 import com.example.wzb.service.JiyixianService;
 import com.example.wzb.service.CommonService;
+import com.example.wzb.util.RedisUtil;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.configurationprocessor.json.JSONObject;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
+import java.util.Map;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
@@ -22,7 +25,7 @@ public class JiyixianController {
     @Autowired
     private JiyixianService indexService;
     @Resource
-    private CommonService<Jiyixian> commonService;
+    private RedisUtil redisUtil;
 /**
  *  @author: wangzongben
  *  @Date: 2021/7/23 10:46
@@ -74,6 +77,29 @@ public class JiyixianController {
         } catch (Exception e) {
             e.printStackTrace();
             return Result.fail(500,e.getMessage());
+        }
+    }
+    @RequestMapping(value = "/redisAdd",method = RequestMethod.POST)
+    @ApiOperation(value = "评论保存",httpMethod = "POST")
+    public Result  redisAdd(@RequestBody Map<String,Object> map){
+
+        try {
+            redisUtil.set("article"+map.get("articleId"),map);
+            return Result.ok(null);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return Result.fail(500,e.getMessage());
+        }
+    }
+    @RequestMapping(value = "/redisList",method = RequestMethod.GET)
+    @ApiOperation(value = "列表方法",httpMethod = "GET")
+    public Result  redisList(String articleId){
+        try {
+            Map o = (Map) redisUtil.get("article" + articleId);
+            return Result.ok(o);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return  Result.fail(500,e.getMessage());
         }
     }
 }
